963cab5bf5bd1bc5ec990799c0fab89b9cb85f26
DPM3 <dpm3git@gmail.com>
2020-09-06 16:48:34 +0300

    realized that we should follow the guidlines, and so we start again

--- src/bfsPathFinder.cpp
+++
@@ -1,9 +0,0 @@
-#include<vector>
-#include<cstdint>
-#include"point.hpp"
-
-
-
-std::vector<Direction> bfsfindPath(Grid grid, Point start, Point end) {
-	return {};
-}
--- src/point.cpp
+++
@@ -1,8 +0,0 @@
-#include"point.hpp"
-
-int& Point::x() {
-	return m_x;
-}
-int& Point::y() {
-	return m_y;
-}
