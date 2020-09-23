9795a82255c022af2c33eb5c4a9c16d7d248d325
DPM3 <dpm3git@gmail.com>
2020-09-23 16:13:58 +0300

    more work on the parser

--- src/operatorID/parser.cpp
+++ src/operatorID/parser.cpp
@@ -1,12 +1,62 @@
 #include"parser.hpp"
 
+#include<cstring>
+#include<string>
+#include<vector>
+
 #include"myTestOperatorID.hpp"
 
 namespace server_side {
 
-OperatorID* parseToOperatorID(std::string content) {
-	(void)content;
-	return new MyTestOperatorID{};
+int OperatorIDParser::parseHeader(std::string const& content) {
+	char* crep = strdup(content.c_str());
+	char* iter = crep;
+
+	std::vector<std::string> tokens(3);
+
+	for (auto& s : tokens) {
+		s = strsep(&iter, " \t\n");
+	}
+
+	if (tokens[0] == "solve") {
+	} else {
+		free(crep);
+		return 2;
+	}
+
+	if (tokens[1] == "find-graph-path") {
+	} else {
+		free(crep);
+		return 2;
+	}
+
+	if (tokens[2] == "BFS") {
+		m_opType = POIDS::BFS;
+	} else if (tokens[2] == "DFS") {
+		m_opType = POIDS::DFS;
+	} else if (tokens[2] == "A*") {
+		m_opType = POIDS::AStar;
+	} else if (tokens[2] == "BestFS") {
+		m_opType = POIDS::BestFS;
+	} else {
+		free(crep);
+		return 2;
+	}
+
+	free(crep);
+	return 0;
+}
+
+OperatorID* OperatorIDParser::parseBody(std::string const& content) {
+	char* crep = strdup(content.c_str());
+	char* iter = crep;
+
+	std::vector<std::string> lines;
+
+	for (char* word; (word = strsep(&iter, "\n")); lines.push_back(word)) { }
+
+	free(crep);
+	return 0;
 }
 
 }
--- src/operatorID/parser.hpp
+++ src/operatorID/parser.hpp
@@ -4,13 +4,19 @@
 
 namespace server_side {
 
-/**
- * @brief parse the given input from client to an OperatorID type.
- */
-//since OperatorID is only an interface,
-//PLEASE NOTE THIS: THIS FUNCTION ALLOCATES MEMORY AND RETURNS IT!
-//YOU MUST BE IN CHARGE OF FREEING THIS MEMORY LATER WITH delete IF YOU CALL THIS FUNCTION!!!!!
-//I know this isn't great, but it is the only way if you work with abstract classes and Interfaces.
-OperatorID* parseToOperatorID(std::string content);
+class OperatorIDParser {
+	enum class PossibleOperatorIDs {
+		BFS, DFS, BestFS, AStar
+	} m_opType;
+	using POIDS = PossibleOperatorIDs;
+public:
+	int parseHeader(std::string const& content);
+
+	//since OperatorID is only an interface,
+	//PLEASE NOTE THIS: THIS FUNCTION ALLOCATES MEMORY AND RETURNS IT!
+	//YOU MUST BE IN CHARGE OF FREEING THIS MEMORY LATER WITH delete IF YOU CALL THIS FUNCTION!!!!!
+	//I know this isn't great, but it is the only way if you work with abstract classes and Interfaces.
+	OperatorID* parseBody(std::string const& content);
+};
 
 }
